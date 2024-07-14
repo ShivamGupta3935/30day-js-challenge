5ee50100a356c8d7ccbced0d8fb26876811f4970
shivam gupta <guptashivam3935@gmail.com>
2024-07-14 11:16:44 +0530

    Day 2 activities 1 completed

---
+++ Day2/Activities1.js
@@ -0,0 +1,19 @@
+//Activities 1 :-- Write a program to add two number and log the result to the console 
+
+let num1 =  123
+let num2 =  25
+
+//Addition of two number
+console.log("Sun of num1 + num2 = ", num1 + num2 );
+
+//Substraction of two number
+console.log( " Substraction of num2 - num1 : ", num1 - num2 );
+
+//Multiplication of two number 
+console.log( "Multiplication of num1 * num2 = ", num1 * num2);
+
+//Devision of num1 and num2 
+console.log( "Devision of num1 and num2 = ", num1 / num2);
+
+// remainder 
+console.log( "remainder of num1 and num2 = ", num1 % num2);
