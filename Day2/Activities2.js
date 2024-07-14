e689a561086fd09c662287b222b5ccc5c01356f1
shivam gupta <guptashivam3935@gmail.com>
2024-07-14 11:42:59 +0530

    Day 2 activities 2 completed

---
+++ Day2/Activities2.js
@@ -0,0 +1,17 @@
+//Assignment operator 
+
+// Task6 use a += operator to add a number to a variable and log the result to the console
+
+let initialNumber = 10 
+let addValue = 13
+let subtVal = 5
+
+initialNumber += addValue
+
+console.log( "Initial Number  : ", initialNumber);
+
+
+// Task  7:-  use the -= operator to substract a number from a variable and log the result 
+ initialNumber -= subtVal
+
+ console.log( "new initail value : ", initialNumber);
